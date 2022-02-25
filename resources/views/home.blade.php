c3da6d4267d3be005ec2064a6a5715edd8c31f0f
Bipin Gyawali <gyawalibipin133@gmail.com>
2022-02-25 20:23:27 +0000

    added design template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+<h1 class="mt-4">Dashboard</h1>
+@endsection
---
+++ resources/views/layouts/partial/_sidebar.blade.php
@@ -0,0 +1,8 @@
+<div class="border-end bg-white" id="sidebar-wrapper">
+    <div class="sidebar-heading border-bottom bg-light">Test App</div>
+    <div class="list-group list-group-flush">
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="{{ route('home') }}">Dashboard</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#">Candidate</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#">Job</a>
+    </div>
+</div>
